88a8dd07055c018459ad29d8e4646a63478125b0
Ramson Noar <yigichwiramson@gmail.com>
2026-09-24 11:44:10 +0100

    configurations

--- config.py
+++ config.py
@@ -15,9 +15,10 @@ class Config:
     GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
     GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET")
 
+    MAIL_SERVER = os.environ.get("MAIL_SERVER")
     MAIL_PORT = int(os.environ.get("MAIL_PORT", "587"))
     MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS", "true").lower() == "true"
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER", MAIL_USERNAME)
-    ADMIN_EMAIL = "yigichwiramson@gmail.com"
+    ADMIN_EMAIL = os.environ.get("ADMIN_EMAIL")
